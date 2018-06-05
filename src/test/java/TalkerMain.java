f6ba15faf08c97af7d28e5bb37d2ea6e1aaf8a45
Michael Bosello <michael.bosello@studio.unibo.it>
2018-06-05 23:13:05 +0200

    conf

--- src/test/java/TalkerMain.java
+++ src/test/java/TalkerMain.java
@@ -14,7 +14,7 @@ import static java.lang.Thread.sleep;
 
 public class TalkerMain{
 
-    private final static int N_MESSAGE = 1000;
+    private final static int N_MESSAGE = 200;
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println("talker process started");
@@ -28,7 +28,7 @@ public class TalkerMain{
         sleep(20000);
         for(int i = 0; i < N_MESSAGE; i++) {
             client.tell(new SendMessage("User" + id + ": message" + i), null);
-            sleep(20);
+            sleep(200);
         }
     }
 }
